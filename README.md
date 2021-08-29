995ee7a14056915b12a83f7f230e81c57f0f9622
Jota Perez <jperez@silentuimapps.com>
2021-08-29 11:50:48 -0300

    :paperclip: Add new alpha version for creaete URL monitor for uptimerobot

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# uptimerobot-addmonitor
-Add monitor to uptimerobot
+# Uptimerobot add URL monitor
+Add monitor to uptimerobot trougth github action
+
+If you don't have an account for uptimerobot please take a look on them site [Oficial site uptimerobot](http://uptimerobot.com)
+
+## Usage
+
+```yaml
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Create URL monitor
+        uses: yojota/uptimerobot-addmonitor@v1
+        with:
+          url_monitor: "https//yojota.cloud"
+          API_uptimerobot: ${{ secrets.API_uptimerobot }}
+
+```
---
+++ new_monitor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+import json
+
+#define MonitorURL and Friendly Name
+myMonitorURL = sys.argv[1]
+friendlyName = "Monitor " + myMonitorURL
+
+#API key for uptimerobot
+API_key = sys.argv[2]
+
+#endpoint
+url = "https://api.uptimerobot.com/v2/newMonitor"
+          
+#concat payload with variables
+payload = "api_key=" + API_key + "&format=json&type=1&url=http%3A%2F%2F"
+payload = payload + myMonitorURL
+payload = payload + '&friendly_name='
+payload = payload + friendlyName
+
+
+headers = {
+    'cache-control': "no-cache",
+    'content-type': "application/x-www-form-urlencoded"
+    }
+          
+response = requests.request("POST", url, data=payload, headers=headers)
+
+data = response.json()
+
+if data['stat'] == 'ok':
+    out_status =  print('Success! ','New monitor to add: ', myMonitorURL, ' Friendly Name: ', friendlyName  )
+    print(out_status)
+elif data['stat'] == 'fail':
+    out_status = ('Failed. Reason', data['error']['message'])
+    print(out_status)
